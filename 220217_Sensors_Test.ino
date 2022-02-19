f7b7ae48f39119c980f0d2bf9c1395b82768c7a8
evanknobler <70674451+evanknobler@users.noreply.github.com>
2022-02-18 21:37:12 -0500

    Add files via upload

--- 220217_Sensors_Test.ino
+++ 220217_Sensors_Test.ino
@@ -7,11 +7,11 @@ const uint16_t DATA_PACKET2_ID = (0x07<<5) | 0x1D;
 const int READ_TIME = 100; // milliseconds
 const int SEND_TIME = 500; // milliseconds
 
-uint16_t PS12volt_raw, PS5volt_raw, PS12curr_raw, PS5curr_raw, PSBATT_raw, odrv1_curr_raw, odrv2_curr_raw, odrv3_curr_raw, temp12_raw, temp5_raw;
-uint8_t PS12volt, PS5volt, PS12curr, PS5curr, PSBATT, odrv1_curr, odrv2_curr, odrv3_curr, temp12, temp5;
+uint16_t PS12volt_raw, PS5volt_raw, PS12curr_raw, PS5curr_raw, PSBATT_raw, odrv1curr_raw, odrv2curr_raw, odrv3curr_raw, main_curr_raw, temp12_raw, temp5_raw;
+uint8_t PS12volt, PS5volt, PS12curr, PS5curr, PSBATT, odrv1curr, odrv2curr, odrv3curr, main_curr, temp12, temp5;
 Chrono read_millis(Chrono::Resolution::MILLIS), send_millis(Chrono::Resolution::MILLIS);
-uint8_t buf1[8];
-uint8_t buf2[8];
+uint8_t buf1[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t buf2[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
 typedef FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> CAN_Type; // TX = 22, RX = 23
 //typedef FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> CAN_Type; // TX = 1, RX = 0
@@ -68,43 +68,54 @@ void loop(){
   can.events();
 
   if (read_millis.hasPassed(READ_TIME)){
-    PS12volt_raw = analogRead(39);
-    PS12volt = (uint8_t)(PS12volt_raw*(12/1023)*10); // raw * scaling factor * 10
-    PS5volt_raw = analogRead(38);
-    PS5volt = (uint8_t)(PS5volt_raw*(5/1023)*10); // raw * scaling factor * 10
-    PS12curr_raw = analogRead(17);
-    PS12curr = (uint8_t)(PS12curr_raw*(12/1023)*10); // raw * scaling factor * 10
-    PS5curr_raw = analogRead(14);
-    PS5curr = (uint8_t)(PS5curr_raw*(5/1023)*10); // raw * scaling factor * 10
     PSBATT_raw = analogRead(40);
-    PSBATT = (uint16_t)(PSBATT_raw*(27/1023)*10); // raw * scaling factor * 10
-  
-    odrv1_curr_raw = analogRead(24);
-    odrv2_curr_raw = analogRead(25);
-    odrv3_curr_raw = analogRead(26);
+    PSBATT = (uint16_t)(PSBATT_raw*(3.3/1023)*(25.9/3.3)*10); // raw * analog scaling * voltage scaling * 10
+    PS12volt_raw = analogRead(39);
+    PS12volt = (uint8_t)(PS12volt_raw*(3.3/1023)*(12/2.5)*10); // raw * analog scaling * voltage scaling * 10
+    PS5volt_raw = analogRead(38);
+    PS5volt = (uint8_t)(PS5volt_raw*(3.3/1023)*(5/2.5)*10); // raw * analog scaling * voltage scaling * 10
+    PS12curr_raw = analogRead(17);
+    PS12curr = (uint8_t)(PS12curr_raw*(3.3/1023)*(12/1.32)*10); // raw * analog scaling * current scaling * 10
+    PS5curr_raw = analogRead(14);
+    PS5curr = (uint8_t)(PS5curr_raw*(3.3/1023)*(5/.55)*10); // raw * analog scaling * current scaling * 10
+    
+    //temp12_raw = analogRead();
+    temp12 = (uint8_t)(10*(.5-temp12_raw*(3.3/1023))/0.01);
+    //temp5_raw = analogRead();
+    temp5 = (uint8_t)(10*(.5-temp5_raw*(3.3/1023))/0.01);;
+    
+    odrv1curr_raw = analogRead(24);
+    odrv1curr = (uint8_t)(odrv1curr_raw*(5/122)*10);
+    odrv2curr_raw = analogRead(25);
+    odrv2curr = (uint8_t)(odrv2curr_raw*(5/122)*10);
+    odrv3curr_raw = analogRead(26);
+    odrv3curr = (uint8_t)(odrv3curr_raw*(5/122)*10);
 
-    temp12_raw = analogRead();
-    temp12 = (uint8_t)(temp12_raw*(125;
-    temp5_raw = analogRead();
-    temp5 = ;
+    main_curr_raw = analogRead(A13);
+    main_curr = (uint8_t)(main_curr_raw*(5/122)*10);
     
     Serial.println("Sensor Values Taken");
     read_millis.restart();
   }
 
   if (send_millis.hasPassed(SEND_TIME)){
-    memcpy(buf1, &PS12volt, 2);
-    memcpy(buf1+2, &PS5volt, 2);
-    memcpy(buf1+4, &PS12curr, 2);
-    memcpy(buf1+6, &PS5curr, 2);
+    memcpy(buf1, &PSBATT, 2);
+    memcpy(buf1+2, &PS12volt, 1);
+    memcpy(buf1+3, &PS5volt, 1);
+    memcpy(buf1+4, &PS12curr, 1);
+    memcpy(buf1+5, &PS5curr, 1);
+    memcpy(buf1+6, &temp12, 1);
+    memcpy(buf1+7, &temp5, 1);
     writeBus(can, buf1, DATA_PACKET1_ID, 8);
     Serial.println("Packet 1 Sent");
-    memcpy(buf2, &PS12volt, 2);
-    memcpy(buf2+2, &PS5volt, 2);
-    memcpy(buf2+4, &PS12curr, 2);
-    memcpy(buf2+6, &PS5curr, 2);
+    
+    memcpy(buf2, &odrv1curr, 1);
+    memcpy(buf2+1, &odrv2curr, 1);
+    memcpy(buf2+2, &odrv3curr, 1);
+    memcpy(buf2+3, &main_curr, 1);
     writeBus(can, buf2, DATA_PACKET2_ID, 8);
     Serial.println("Packet 2 Sent");
+    
     send_millis.restart();
   }
     
